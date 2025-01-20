f5c7bc82a99e8c26922506e212863c4641428df4
Hamish McLean <HamishMcLean94@gmail.com>
2025-01-20 22:15:46 +0000

    Updated nix-on-droid config

--- hosts/localhost.nix
+++ hosts/localhost.nix
@@ -3,7 +3,8 @@
   This imports Cycad's default home manager module which imports the other modules.
   Modules can be enabled or disabled here.
 */
-{ 
+{
+  username,
   ...
 }:
 {
@@ -16,4 +17,9 @@
 
   # Disable specific modules
 
+
+  home = {
+    username = username;
+    homeDirectory = "data/data/com.termux.nix/files/home";
+  };
 }
