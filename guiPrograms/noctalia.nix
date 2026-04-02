9659b1bf4ba7b908614cb4928f915dbb7643d000
Hamish McLean <HamishMcLean94@gmail.com>
2026-04-02 23:16:31 +0100

    Refactor and tweak noctalia

--- guiPrograms/noctalia.nix
+++ guiPrograms/noctalia.nix
@@ -21,56 +21,81 @@
 
     # Settings
     programs.noctalia-shell.settings = {
-      appLauncher.position = "top_left";
+      appLauncher.position = "top_center";
       bar.showCapsule = false;
       bar.widgets = {
         left = [
-          { id = "Launcher"; }
+          {
+            id = "ControlCenter";
+            useDistroLogo = true;
+          }
+          # { id = "Launcher"; }
           { id = "plugin:workspace-overview"; }
           {
             id = "Workspace";
             showApplications = true;
           }
-          { id = "ActiveWindow"; }
+          # { id = "ActiveWindow"; }
         ];
         center = [
-          { id = "MediaMini"; }
-        ];
-        right = [
-          { id = "plugin:catwalk"; }
-          { id = "SystemMonitor"; }
-          { id = "Tray"; }
-          { id = "kde-connect"; }
-          { id = "plugin:tailscale"; }
-          { id = "Network"; }
-          { id = "Bluetooth"; }
-          {
-            id = "Battery";
-            displayMode = "alwaysShow";
-            showPowerProfiles = true;
-          }
           {
             id = "Clock";
             formatHorizontal = "HH:mm ddd yyyy-MM-dd";
             tooltipFormat = "HH:mm ddd yyyy-MM-dd";
           }
           { id = "plugin:weather-indicator"; }
+          # {
+          #   id = "MediaMini";
+          #   hideMode = "idle";
+          # }
+        ];
+        right = [
+          { id = "plugin:catwalk"; }
+          { id = "SystemMonitor"; }
+          { id = "Spacer"; }
           {
             id = "NotificationHistory";
             hideWhenZero = true;
           }
+          { id = "Tray"; }
+          { id = "plugin:kde-connect"; }
+          { id = "plugin:tailscale"; }
+          { id = "plugin:syncthing-status"; }
+          { id = "Network"; }
+          { id = "Bluetooth"; }
           {
-            id = "ControlCenter";
-            useDistroLogo = true;
+            id = "Battery";
+            displayMode = "graphic";
+            hideIfIdle = false;
+            showNoctaliaPerformance = true;
+            showPowerProfiles = true;
+          }
+          {
+            id = "CustomButton";
+            icon = "settings";
+            leftClickExec = "noctalia-shell ipc call settings toggle";
           }
         ];
       };
       colorSchemes.predefinedScheme = "Catppuccin Custom"; # switch to custom theme with accent
+      dock = {
+        enabled = true;
+        dockType = "attached";
+        showLauncherIcon = true;
+      };
       general = {
+        animationDisabled = false;
+        clockFormat = "hh:mm\nyyyy-MM-dd";
+        compactLockScreen = false;
+        enableLockScreenMediaControls = true;
+        enableShadows = true;
         forceBlackScreenCorners = true;
+        passwordChars = true;
+        screenRadiusRatio = 0.8;
+        showHibernateOnLockScreen = true;
         showScreenCorners = true;
       };
-      location.name = "Tunbridge Wells";
+      location.name = "Totnes";
       ui.panelBackgroundOpacity = 1;
       wallpaper = {
         enabled = true;
@@ -92,7 +117,10 @@
       states = {
         battery-actions.enabled = true;
         catwalk.enabled = true;
+        keybind-cheatsheet.enabled = true;
         kde-connect.enabled = true;
+        niri-overview-launcher.enabled = true;
+        syncthing-status.enabled = true;
         tailscale.enabled = true;
         weather-indicator.enabled = true;
         workspace-overview.enabled = true;
@@ -100,6 +128,10 @@
       version = 2;
     };
     programs.noctalia-shell.pluginSettings = {
+      battery-actions = {
+        onBatteryScript = "noctalia-shell ipc call powerProfile set \"balanced\"";
+        pluggedInScript = "noctalia-shell ipc call powerProfile set \"performance\"";
+      };
       tailscale = {
         compactMode = true;
         terminalCommand = "kitty";
